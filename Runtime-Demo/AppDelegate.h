2fb53972bf408cfb4e1e27982d550087b2825511
dong.wang <dong.wang@yingzt.com>
2019-05-08 15:28:01 +0800

    Initial Commit

---
+++ Runtime-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runtime-Demo
+//
+//  Created by 王泽龙 on 2019/5/8.
+//  Copyright © 2019 王泽龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
